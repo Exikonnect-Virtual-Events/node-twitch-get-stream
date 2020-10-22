69890db6ffb6ec11423123c0689748cecbc61718
Smith Pereira <capriciousrebel@Smiths-Macbook-Pro.local>
2020-10-22 17:19:13 +0530

    smith changes

---
+++ getChannelStreams.js
@@ -0,0 +1,25 @@
+var twitchStreams = require('.');
+const prompt = require('prompt');
+
+prompt.start();
+console.log("Enter the name of twitch Channel: ")
+prompt.get(['channelName'], function (err, result) {
+    if (err) { return onErr(err); }
+    twitchStreams.get(result.channelName)
+        .then(function (streams) {
+            for (var stream of streams) {
+                if (stream.resolution === '1280x720' && stream.quality === '720p60') {
+                    console.log('m3u8 Link: ' + stream.url);
+                }
+            }
+        })
+        .catch(function (error) {
+            if (error)
+                return console.log('Error caught:', error);
+        });
+});
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
